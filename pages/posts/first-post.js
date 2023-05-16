8e8099987242d837116b5fb0c2e830880bd46db7
Adam Iverson <aciverso@gmail.com>
2023-05-16 10:54:41 -0500

    image displaying on first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FirstPost() {
   return (
@@ -6,6 +7,12 @@ export default function FirstPost() {
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
+        <Image
+              src="/../public/images/placeholder.jpeg" // Route of the image file
+              height={144} // Desired size with correct aspect ratio
+              width={144} // Desired size with correct aspect ratio
+              alt="Your Name"
+        />
       </h2>
     </>
   );
